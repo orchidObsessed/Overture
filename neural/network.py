e565bdf9a73aacdd318f28b12f9ea6632323bbae
orchidObsessed <william.waddell0507@gmail.com>
2022-10-05 00:01:19 -0500

    Begun work on training functionality, needs backprop & update

--- neural/network.py
+++ neural/network.py
@@ -6,6 +6,7 @@
 from helpers import sapilog as sl
 from neural import layer
 import numpy as np
+from random import randint
 # ===== < BODY > =====
 class NNetwork:
     """
@@ -14,6 +15,7 @@ class NNetwork:
     def __init__(self):
         self._raw_layers = []
         self._weights = [] # List of NumPy ndarrays representing weight matrices
+        return
 
     # +----------------+
     # |    Mutators    |
@@ -35,6 +37,35 @@ class NNetwork:
         self._weights.append(w)
         return
 
+    def train(self, train_data: list[np.array], label_data: list[np.array], c_func: callable, q_c_func: callable, batch_size: int, n_epochs: int, learning_rate=0.1, report_freq=1):
+        """
+
+        """
+        # Step 0: Set up local variables and log
+
+        # Step 1: Main training loop
+        for e in range(n_epochs):
+            # Step 1a: Get sample
+            sample_index = randint(0, len(train_data))
+            x, y = train_data[sample_index], label_data[sample_index]
+            sl.log(4, f"Considering sample {sample_index}: {x} -> {y}")
+
+            # Step 1b: Feed forward, and retain activations and weighted inputs
+            self.feedforward(x)
+            activations, zs = [l.a for l in self._raw_layers[1:]], [l.z for l in self._raw_layers[1:]]
+            sl.log(4, f"Activations:\n{activations}\n\nWeighted inputs:\n{zs}")
+
+            # Step 1c: Get the loss of the evaluation for this sample
+            loss = c_func(activations[-1], y)
+            sl.log(4, f"Loss: {loss}")
+
+            # Step 1d: Calculate gradient for output layer
+            delta = zs[-1]
+
+        # Step 2: Report accuracy
+
+        return
+
     # +----------------+
     # |   Accessors    |
     # +----------------+
@@ -46,7 +77,7 @@ class NNetwork:
 
     def feedforward(self, x: np.array):
         """
-
+        Feed a value through the network, and return the final layer's activation as a NumPy nx1 ndarray.
         """
         a = x # Force input activation
         for l, w in zip(self._raw_layers[1:], self._weights):
