9a2fb109d2aa03348a59d7d002983b6e1672805f
orchidObsessed <william.waddell0507@gmail.com>
2022-10-05 00:01:00 -0500

    Added retention of activation and weighted input, added derivative func

--- neural/layer.py
+++ neural/layer.py
@@ -9,10 +9,12 @@ class Layer:
     """
     Base layer class.
     """
-    def __init__(self, size: int, a_func: callable):
+    def __init__(self, size: int, a_func: callable, q_a_func: callable):
         self._size = size
         self._b = np.zeros((size, 1)) # Sad that this needs to be mat instead of vec
         self._a_func = a_func
+        self._q_a_func = q_a_func
+        self.z, self.a = None, None
         return
 
     def activation(self, x: np.array, w: np.array) -> np.array:
@@ -26,12 +28,15 @@ class Layer:
         `w` : np.array
             Weights between previous layer and here as a NumPy ndarray of floats.
         """
-        a = w.T @ x + self._b
-        return a
+        sl.log(4, f"Calculating activation with input {x} and weight matrix {w}")
+        self.z = w.T @ x + self._b
+        self.a = self._a_func(self.z)
+        sl.log(4, f"Created z={self.z} and a={self.a}")
+        return self.a
 
     def __len__(self):
         return self._size
-    
+
 # ===== < HELPERS > =====
 
 # ===== < MAIN > =====
