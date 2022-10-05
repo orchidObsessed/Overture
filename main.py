56afa92c8abc65685864fc857b6bce12eb9a00c9
orchidObsessed <william.waddell0507@gmail.com>
2022-10-05 00:00:30 -0500

    Updated main with additional testing

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # ===== < INFO > =====
 
 # ===== < IMPORTS & CONSTANTS > =====
+from helpers import sapilog as sl
+from helpers import datagen
+from helpers import algebra as alg
 from neural import layer as l
 from neural import network as nn
 import numpy as np
@@ -10,12 +13,23 @@ import numpy as np
 
 # ===== < MAIN > =====
 if __name__ == "__main__":
+    # Test some stuff
+    # print(alg.sigmoid(np.array([-5, 100, 0])))
+    # print(alg.q_sigmoid(np.array([-5, 100, 0])))
+    # print(alg.mse(np.array([0, 0, 0]), np.array([0, 1, 0])))
+    # print(alg.q_mse(np.array([0, 0, 0]), np.array([0, 1, 0])))
+
+    # Read and format test data
+    fold1, fold2, fold3 = datagen.collectData("d3&c25&s200")
+    train_data, label_data = list(fold1.keys()), list(fold1.values())
+    train_data = [np.array(x).reshape(2, 1) for x in train_data]
+    label_data = [np.array(x) for x in label_data]
+
+    # Create network
     test_network = nn.NNetwork()
-    test_network += l.Layer(2, lambda x: x)
-    test_network += l.Layer(3, lambda x: x)
-    test_network += l.Layer(1, lambda x: x)
+    test_network += l.Layer(2, alg.sigmoid, alg.q_sigmoid)
+    test_network += l.Layer(3, alg.sigmoid, alg.q_sigmoid)
+    test_network += l.Layer(1, alg.sigmoid, alg.q_sigmoid)
 
-    test_x = np.array([[2], [2]]) # Sample input
-
-    output = test_network.feedforward(test_x)
-    print(f"Input {test_x}\nyields output {output}")
+    # Train test network
+    test_network.train(train_data, label_data, alg.mse, alg.q_mse, 0, 1)
