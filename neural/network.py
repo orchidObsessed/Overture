02858efc8f9a256ed8c3a8a3ad777e693ef37680
orchidObsessed <william.waddell0507@gmail.com>
2022-10-06 14:12:14 -0500

    Added batching to the train function

--- neural/network.py
+++ neural/network.py
@@ -39,7 +39,26 @@ class NNetwork:
 
     def train(self, train_data: list[np.array], label_data: list[np.array], c_func: callable, q_c_func: callable, batch_size: int, n_epochs: int, learning_rate=0.1, report_freq=1):
         """
+        Train the network using stochastic gradient descent.
 
+        Parameters
+        ----------
+        `train_data` : list[np.array]
+            List of NumPy ndarrays representing datapoints in the training pool
+        `label_data` : list[np.array]
+            List of NumPy ndarrays representing labels for training data
+        `c_func` : callable
+            Cost / loss function
+        `q_c_func` : callable
+            Derivative of `c_func`
+        `batch_size` : int
+            Number of samples per batch
+        `n_epochs` : int
+            Number of batches to run
+        `learning_rate` : float
+            Learning rate to be applied to the weight gradients
+        `report_freq` : int
+            How many epochs before a report is generated
         """
         # Step 0: Set up local variables and log
         sl.log(3, f"Beginning training with {len(train_data)} samples over {n_epochs} epochs, using batch size {batch_size} and a learning rate of {learning_rate}")
@@ -47,43 +66,61 @@ class NNetwork:
         # Step 1: Main training loop
         for e in range(n_epochs):
             if e % report_freq == 0: print(f"Epoch {e}")
-            # Step 1a: Get sample
-            sample_index = randint(0, len(train_data)-1)
-            x, y = train_data[sample_index], label_data[sample_index]
-            sl.log(4, f"Considering sample {sample_index}: {x} -> {y}")
 
-            # Step 1b: Feed forward, and retain activations and weighted inputs
-            self.feedforward(x)
-            activations, zs = [l.a for l in self._raw_layers[1:]], [l.z for l in self._raw_layers[1:]]
-            activations.insert(0, x)
+            batch_w_grad, batch_b_grad = [np.zeros_like(w) for w in self._weights], [np.zeros_like(l.b) for l in self._raw_layers[1:]]
 
-            # Step 1c: Get the loss of the evaluation for this sample
-            loss = c_func(activations[-1], y)
+            for b in range(batch_size):
+                # Step 1a: Get sample
+                sample_index = randint(0, len(train_data)-1)
+                x, y = train_data[sample_index], label_data[sample_index]
 
-            # Step 1d: Calculate gradient for output layer
-            sample_w_grad = []
+                # Step 1b: Feed forward, and retain activations and weighted inputs
+                self.feedforward(x)
+                activations, zs = [l.a for l in self._raw_layers[1:]], [l.z for l in self._raw_layers[1:]]
+                activations.insert(0, x)
 
-            local_gradient = q_c_func(activations[-1], y)
-            del_w = activations[-2] @ local_gradient
+                # Step 1c: Get the loss of the evaluation for this sample
+                loss = c_func(activations[-1], y)
 
-            sample_w_grad.append(del_w)
+                # Step 1d: Calculate gradient for output layer
+                sample_w_grad, sample_b_grad = [], []
 
-            # Step 1e: Propagate for rest of network
-            for l, w in zip(reversed(range(1, len(self._raw_layers)-1)), reversed(range(len(self._weights)))):
-                local_gradient = self._weights[w] @ local_gradient # Propagate local gradient back on weights
+                local_gradient = q_c_func(activations[-1], y)
+                del_w = activations[-2] @ local_gradient
+                del_b = local_gradient
 
-                del_w = activations[l-1] @ local_gradient.T
-                sample_w_grad.insert(0, del_w)
+                sample_w_grad.append(del_w)
+                sample_b_grad.append(del_b)
 
-            # Step 1f: Apply with learning rate to weights (should be changed to apply to megabatch)
-            for w, dw in zip(range(len(self._weights)), sample_w_grad):
-                self._weights[w] = self._weights[w] - dw * learning_rate
+                # Step 1e: Propagate for rest of network
+                for l, w in zip(reversed(range(1, len(self._raw_layers)-1)), reversed(range(len(self._weights)))):
+                    local_gradient = self._weights[w] @ local_gradient # Propagate local gradient back on weights
+
+                    del_w = activations[l-1] @ local_gradient.T
+                    del_b = local_gradient
+
+                    sample_w_grad.insert(0, del_w)
+                    sample_b_grad.insert(0, del_b)
+
+                # Step 1f: Apply to batch gradient
+                for w, dw in zip(range(len(batch_w_grad)), sample_w_grad):
+                    batch_w_grad[w] = batch_w_grad[w] + dw
+
+                for b, db in zip(range(len(batch_b_grad)), sample_b_grad):
+                    batch_b_grad[b] = batch_b_grad[b] + db
+
+            # Step 1g: Apply alongside learning rate to weights and biases
+            for w, dw in zip(range(len(self._weights)), batch_w_grad):
+                self._weights[w] = self._weights[w] - dw * (learning_rate/batch_size)
+
+            for l, db in zip(self._raw_layers[1:], batch_b_grad):
+                l.b = l.b - db * (learning_rate/batch_size)
 
 
         # Step 2: Report accuracy
         n_correct = 0
         for i in range(50):
-            sample_index = randint(0, len(train_data))
+            sample_index = randint(0, len(train_data)-1)
             x, y = train_data[sample_index], label_data[sample_index]
 
             guess = self.feedforward(x)
@@ -111,6 +148,16 @@ class NNetwork:
         for l, w in zip(self._raw_layers[1:], self._weights):
             a = l.activation(a, w)
         return a
+
+    def tell_params(self):
+        """
+        Logs network parameters.
+        """
+        sl._logBox("PARAMETERS")
+        sl.log(4, f"Shape: {[len(l) for l in self._raw_layers]}")
+        sl.log(4, f"Biases: {[l.b for l in self._raw_layers]}")
+        sl.log(4, f"Weights: {self._weights}")
+        return
 
 # ===== < HELPERS > =====
 
