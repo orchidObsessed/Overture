a135a1c489f7a4990050b62119d2c8be4177be41
OrchidObsessed <william.waddell0507@gmail.com>
2022-10-06 16:22:06 -0500

    Read validation data from fold2 for evaluation of network after training

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ from helpers import algebra as alg
 from neural import layer as l
 from neural import network as nn
 import numpy as np
+from time import sleep
 
-sl.vtalk = 3
+sl.vtalk = 4
 sl.vwrite = 4
 # ===== < BODY > =====
 
@@ -22,14 +23,21 @@ if __name__ == "__main__":
     train_data = [np.array(x).reshape(2, 1) for x in train_data]
     label_data = [np.array(x) for x in label_data]
 
+    # Format validation data
+    validata, valilabel = list(fold2.keys()), list(fold2.values())
+    validata = [np.array(x).reshape(2, 1) for x in validata]
+    valilabel = [np.array(x) for x in valilabel]
+
     # Create network
     test_network = nn.NNetwork()
     test_network += l.Layer(2, alg.sigmoid, alg.q_sigmoid)
-    test_network += l.Layer(3, alg.sigmoid, alg.q_sigmoid)
-    test_network += l.Layer(4, alg.sigmoid, alg.q_sigmoid)
+    # test_network += l.Layer(3, alg.sigmoid, alg.q_sigmoid)
     test_network += l.Layer(1, alg.sigmoid, alg.q_sigmoid)
 
     # Train test network
     test_network.tell_params()
-    test_network.train(train_data, label_data, alg.mse, alg.q_mse, 50, 100, report_freq=25)
+    test_network.train(train_data, label_data, alg.mse, alg.q_mse, 50, 1000, report_freq=25)
     test_network.tell_params()
+
+    # Evaluate network
+    test_network.evaluate(validata, valilabel, alg.mse)
