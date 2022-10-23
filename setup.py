2213589716803bac7149edbe46d8a370444b4e9c
orchidObsessed <william.waddell0507@gmail.com>
2022-10-23 10:28:41 -0500

    A (probably broken) setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "Overture",
+    version = "1.2.1",
+    description = "A from-scratch neural network library, using only NumPy as a fast matrix implementation.",
+    author = "William \"Waddles\" Waddell",
+    author_email = "william.waddell0507@gmail.com",
+    packages = ["helpers", "neural", "helpers.logsuite"]
+)
