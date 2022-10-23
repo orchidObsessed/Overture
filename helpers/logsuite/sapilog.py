02481929326eb20bbf68f613dea4a92319c5a4ab
orchidObsessed <william.waddell0507@gmail.com>
2022-10-23 10:22:19 -0500

    Added logsuite to this projec to allow for faster log messages

---
+++ helpers/logsuite/sapilog.py
@@ -0,0 +1,147 @@
+# ===== < MODULE INFO > =====
+# Author: William "Waddles" Waddell
+# Version: 2.1.4
+# Description: A smarter logging platform combining live output and logfile functions.
+# ===== < IMPORTS & CONSTANTS > =====
+import inspect, os, functools, sys, threading
+from time import strftime, sleep
+from logsuite import velocius as vl
+
+# Formatting constants
+COLOR = {"RED":"\033[31m",
+          "YELLOW":"\033[33m",
+          "GREEN":"\033[32m",
+          "BLUE":"\033[36m",
+          "MAGENTA":"\033[35m",
+          "GRAY":"\033[90m",
+          "CLEAR":"\033[0m"} # Color codes, for easier referencing
+FLAGS = {0:f"<{COLOR['RED']}FAIL{COLOR['CLEAR']}>",
+         1:f"<{COLOR['YELLOW']}WARN{COLOR['CLEAR']}>",
+         2:f"<{COLOR['GREEN']}GOOD{COLOR['CLEAR']}>",
+         3:f"<{COLOR['BLUE']}INFO{COLOR['CLEAR']}>",
+         4:f"<{COLOR['MAGENTA']}DBUG{COLOR['CLEAR']}>"} # Prebaked verbosity prefixes
+MONO_FLAGS = {0: "<FAIL>",
+           1: "<WARN>",
+           2: "<GOOD>",
+           3: "<INFO>",
+           4: "<DBUG>"} # Monochrome verbosity prefixes (for mono-mode logging, or logwrite)
+
+# Log-level constants
+MAX_V_PRINT = 2 # Highest verbosity level to print at
+MAX_V_WRITE = 3 # Highest verbosity level to write at
+MONO = False # Whether to print using color (written logs never use color)
+
+# Logger vars
+QUEUE = []
+UNIV_LOG_LOCK = threading.Lock()
+MAX_QUEUE_LEN = 10 # Max number of lines to save in the queue
+MAX_QUEUE_MEM = 32000 # Max number of bytes queue can take up
+MAX_LOG_MEM = 1000000 # Max memory size of logfile before warning
+LOG_PATH = "logs/" # Where to write logs to
+
+# ===== < BODY > =====
+@vl.pooled_threaded
+def log(verbosity: int, message: int, stack: list = None):
+    """
+    Log an event with a message and a verbosity (severity) level.
+
+    Parameters
+    ----------
+    `verbosity` : int
+        Verbosity level for this message (0=fail,1=warn,2=good,3=info,4=dbug)
+    `message` : str
+        Message to print
+    `stack` : list, optional
+        Stack trace at time of call.
+
+    Notes
+    -----
+    Looking for a way to automatically get the stack trace; the introduction of multithreading has made logging much more viable, but harder to trace.
+    If `stack` is not present, log will fill in missing and warn.
+    """
+    # Step 0: If verbosity is too low, skip
+    if verbosity > MAX_V_WRITE and verbosity > MAX_V_PRINT: return True
+
+    # Step 1: Stack trace for log header
+    if stack:
+        caller_function = str(stack[1].function) # This will retrieve the function from which this was called
+        if caller_function == "<module>": caller_function = "__main__"
+        caller_location = str(stack[1].filename.split("\\")[-1]) # This will retrieve the module that contains that function
+    else:
+        # log(1, "Stack trace was not given; using filler", inspect.stack())
+        caller_location, caller_function = "?.py", "?"
+
+    # Step 2: Format and build logstrings
+    for punc in [".", ",", "!", "?"]: message = message.rstrip(punc)
+    full_color = f"{FLAGS[verbosity]}{COLOR['GRAY']}:{caller_location}:{caller_function}()->{COLOR['CLEAR']}{message}"
+    full_monochrome = f"{MONO_FLAGS[verbosity]}:{caller_location}:{caller_function}()->{message}"
+
+    # Step 3: Tell and enqueue, under with to prevent race condi
+    with UNIV_LOG_LOCK:
+        global QUEUE
+        if verbosity <= MAX_V_PRINT:
+            if MONO: print(full_monochrome)
+            else: print(full_color)
+        else: print(f"Verbosity {verbosity} > max print {MAX_V_PRINT}")
+        if verbosity <= MAX_V_WRITE: QUEUE.append(f"|{strftime('%d-%m-%y %H:%M:%S')}| {full_monochrome}")
+
+        if len(QUEUE) >= MAX_QUEUE_LEN or sys.getsizeof(QUEUE) >= MAX_QUEUE_MEM: _log_dump()
+
+    return True
+
+# ===== < HELPERS > =====
+def _logBox(m):
+    """
+
+    Write a message `m` within a box, using the DBUG flag.
+    +--------------------+
+    |  Message in a box! |
+    +--------------------+
+    """
+    if len(m) > 20: m = m[:20]
+    wspace = int((20-len(m))/2) # This gets floored to preserve hard 20 char limit
+    if len(m) % 2 == 0: m = "|" + " "*wspace + m + " "*wspace + "|"
+    else: m = "|" + " "*wspace + m + " "*wspace + " |"
+    log(4, "\t\t+--------------------+")
+    log(4, "\t\t"+m)
+    log(4, "\t\t+--------------------+")
+    return
+
+def _log_dump():
+    global QUEUE
+
+    # Filepath generation & verification
+    fpath = f"{LOG_PATH}{strftime('%m-%d-%y')}.slog"
+    if not os.path.exists(fpath):
+        with open(fpath, "a+") as _: pass
+    if os.path.getsize(fpath) > 1000000: log(1, "Slog file exceeding 1MB, recommend regeneration". inspect.stack())
+
+    # Dump queue
+
+    with open(fpath, "a+") as fo:
+        fo.writelines([x+"\n" for x in QUEUE])
+        fo.write("*** "*30 + "\n")
+    QUEUE = []
+    return
+
+# ===== < EXCEPTIONS > =====
+class SapiException(Exception):
+    """
+    Base exception for `sapilog` exceptions. All exception classes derived from this will dump the log queue when raised.
+    """
+    def __init__(self):
+        pass
+
+# ===== < MAIN > =====
+if __name__ == "__main__":
+    log(0, "This is a sample failure")
+    sleep(0.5)
+    log(1, "This is a sample warning")
+    sleep(0.5)
+    log(2, "This is a sample success message")
+    sleep(0.5)
+    log(3, "This is a sample info message")
+    sleep(0.5)
+    log(4, "This is a sample debug message")
+    sleep(0.5)
+    _logBox("now tabbed for visibility")
---
+++ helpers/logsuite/velocius.py
@@ -0,0 +1,262 @@
+# ===== < MODULE INFO > =====
+# Author: William "Waddles" Waddell
+# Version: 2.1.4
+# Description: A small threading library using pooling to allow for faster execution of repetitive functions.
+# ===== < IMPORTS & CONSTANTS > =====
+import threading as t
+from queue import Queue, Empty
+import functools, inspect
+
+UNIQUE_POOLS = {}
+# ===== < BODY > =====
+class OldLagoon:
+    """
+
+    """
+    NAMECOUNTER = 0
+    def __init__(self, name="Lagoon", n_procs=1, max_size=0):
+        self.name = name + "_" + str(Lagoon.NAMECOUNTER)
+        Lagoon.NAMECOUNTER += 1
+        self.queue = Queue(maxsize=max_size)
+        self.procs = []
+        self.spawn_new(n_procs)
+
+    def enqueue(self, fargs_tuple):
+        """
+
+        """
+        self.queue.put(fargs_tuple)
+        return
+
+    def spawn_new(self, n=1):
+        """
+        Spawn `n` new multiprocessing.Process processes in this pool.
+        """
+        for i in range(n):
+            self.procs.append(t.Thread(target=self.__tick))
+            self.procs[-1].start()
+        return
+
+    def __tick(self, killcycle=False):
+        """
+
+        """
+        if not killcycle: end = not t.main_thread().is_alive()
+        else: end = self.queue.empty()
+        while not end:
+            fcall = self.queue.get() # In form: (f, *args) || (!break, n)
+            if fcall[0] == "!break":
+                self.queue.task_done()
+                if fcall[1] > 1: self.queue.put(("!break", fcall[1]-1))
+                end, killcycle = True, True
+            else:
+                try: fcall[0](*fcall[1])
+                except NameError as e: sl.log(0, f"NameError in queued function {fcall[0]} with provided arguments {fcall[1]} do not exist; {e}", inspect.stack())
+                except TypeError as e: sl.log(0, f"TypeError in queued function {fcall[0]} with given arguments {fcall[1]}; {e}", inspect.stack())
+                except Exception as e: sl.log(0, f"Unknown error occured with queued function {fcall[0]} and arguments {fcall[1]}; {e}", inspect.stack())
+                finally: self.queue.task_done()
+            if not killcycle: end = not t.main_thread().is_alive()
+            else: end = self.queue.empty()
+        if not killcycle: self.__tick(killcycle=True)
+        return
+
+    def join_and_kill(self, n=None):
+        """
+        Kill `n` processes. If `n` is not present, kills all processes.
+        """
+        if n: self.queue.put(("!break", n))
+        else: self.queue.put(("!break", len(self.procs)))
+        self.queue.join()
+        return
+
+class OvercomplicatedLagoon:
+    """
+
+    """
+    NAME_COUNTER = 0
+
+    def __init__(self, n_threads=1, block_time=1):
+        self.name = f"Lagoon_{Lagoon.NAME_COUNTER}"
+        print(f"Starting construction of Lagoon {self.name}")
+        Lagoon.NAME_COUNTER += 1
+
+        self.queue = Queue()
+        self.block_time = block_time
+
+        self.size = n_threads # Number of threads at start
+        self.__procs = []
+        self.spawn(self.size)
+        print(f"Spawn finished!")
+        self.spin_up()
+        print(f"Lagoon with name {self.name} created")
+
+    def enqueue(self, fargs):
+        """
+        Add a new item to the queue for this pool.
+
+        Parameters
+        ----------
+        `fargs` : tuple
+            Tuple containing callable function at index 0
+
+        Notes
+        -----
+        This is currently unsafe due to lack of support for pure-args, pure-kwargs, and mixed argtypes.
+        """
+        print(f"Lagoon with name {self.name} is putting {fargs} in the queue")
+        try: self.queue.put(fargs)
+        except Exception as e: print(f"Something went wrong when enqueueing item | {e}")
+        finally: return
+
+    def spawn(self, n: int = 1):
+        """
+        Add a new thread to this pool to increase performance for the given function.
+
+        Parameters
+        ----------
+        `n` : int, default=1
+            Number of new threads to add
+        """
+        print(f"Lagoon with name {self.name} trying to spawn {n} threads...")
+        for i in range(n):
+            self.__procs.append(t.Thread(target=self.__main()))
+        print(f"Lagoon with name {self.name} spawned {n} threads")
+        return
+
+    def spin_up(self):
+        """
+
+        """
+        for proc in self.__procs:
+            try:
+                proc.start()
+            except Exception as e:
+                print(f"Process was likely already running | {e}")
+
+        print(f"Lagoon with name {self.name} started all threads")
+        return
+
+    def spin_down(self):
+        """
+
+        """
+        for i in range(self.size): self.enqueue("!")
+        self.queue.join() # Block until queue is empty
+        return
+
+    def despawn(self, n: int = 0):
+        """
+        Begin graceful termination and destruction of threads in this pool.
+
+        Parameters
+        ----------
+        `n` : int, optional
+            Number of threads to terminate. If 0, all threads are terminated.
+        """
+        for i in range(n):
+            self.enqueue("!")
+        return
+
+    def __main(self):
+        """
+        Thread's main (identity) loop.
+        """
+        print(f"Lagoon with name {self.name} begun main loop")
+        while True:
+            state = self.__work_tick() # Do a work tick and get its return code
+            if state == -1:
+                print(f"Thread {self.name} ended its work tick with code -1; aborting")
+                sys.exit()
+            elif state == 2:
+                print(f"Thread {self.name} ended its work tick with code 2; starting cleanup")
+                return
+            elif state == 3:
+                print(f"Thread {self.name} recieved termination signal; starting cleanup")
+                return
+            print(f"Lagoon with name {self.name} finished main loop")
+        return
+
+    def __work_tick(self):
+        """
+        Work tick.
+
+        Attempts to get item from queue and complete it, then returns to the main loop.
+
+        Returns
+        -------
+        -1 : Something went horribly wrong
+        0  : Function was retrieved and executed successfully
+        1  : No function was retrieved before timeout
+        2  : Function was retrieved and failed to execute
+        3  : Graceful termination signal recieved
+        """
+        end_state = -1 # Default state; this should never be returned
+        try:
+            fcall = self.queue.get(block=True, timeout=self.block_time) # Get item from queue
+            if fcall[0] == "!": end_state = 3
+            else:
+                fcall[0](*fcall[1])
+                end_state = 0
+        except Empty as _:
+            end_state = 1
+        except Exception as e:
+            print(f"{self.name} encountered error {e}")
+            end_state = 2
+        finally:
+            if not self.queue.empty(): self.queue.task_done()
+        print(f"Lagoon with name {self.name} finished work tick with code {end_state}")
+        return end_state
+
+class SimpleLagoon:
+    """
+    Pooled, uni-threaded task queue.
+    """
+    def __init__(self, fname: str):
+        self.name = str(fname)
+        self.__task_queue = Queue()
+        self.__proc = t.Thread(target=self.__main).start()
+        self.__block_queue = False
+
+    def __main(self):
+        main_is_alive = t.main_thread().is_alive()
+        queue_is_full = self.__task_queue.full()
+        while main_is_alive or queue_is_full:
+            print(f"{main_is_alive} | {queue_is_full}")
+            try:
+                fcall = self.__task_queue.get(block=True, timeout=1) # Function call
+                if fcall[0] == "!":
+                    break
+                else:
+                    fcall[0](*fcall[1])
+                    self.__task_queue.task_done()
+            except Empty as _: pass
+            except Exception as e: print(f"Error in Lagoon {self.name}: {e}")
+            finally:
+                main_is_alive = t.main_thread().is_alive()
+                queue_is_full = self.__task_queue.full()
+        return
+
+    def enqueue(self, fargs):
+        if not self.__block_queue: self.__task_queue.put(fargs)
+        else:
+            print(f"{fargs} is not being queued because this lagoon is slated for termination!")
+        return
+
+def pooled_threaded(func):
+    """
+    Carry out the decorated function in a thread; do not wait if no threads are available (raises `Exception`).
+
+    This does not return the output of the function.
+    """
+    global UNIQUE_POOLS
+    if func not in UNIQUE_POOLS.keys():
+        UNIQUE_POOLS[func] = SimpleLagoon(func)
+    thispool = UNIQUE_POOLS[func]
+    @functools.wraps(func)
+    def wrapper_doInThread(*args):
+        thispool.enqueue((func, args))
+    return wrapper_doInThread
+
+# ===== < MAIN > =====
+if __name__ == "__main__":
+    print(f"This shouldn't have been run!")
