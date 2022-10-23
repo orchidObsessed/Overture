6a217a2c421515cbcf5e678910c3a8b75ecdba2e
orchidObsessed <william.waddell0507@gmail.com>
2022-10-23 12:07:10 -0500

    Added different test stuff

--- main.py
+++ main.py
@@ -9,14 +9,17 @@ from neural import network as nn
 import numpy as np
 from time import sleep
 
-sl.vtalk = 4
-sl.vwrite = 4
+sl.MAX_V_PRINT = 4
+sl.MAX_V_WRITE = 0 # Don't want to be writing to log for now
 # ===== < BODY > =====
 
 # ===== < HELPERS > =====
 
 # ===== < MAIN > =====
 if __name__ == "__main__":
+    # TEST - Set all warnings to raise as errors instead
+    # np.seterr(all="raise")
+
     # Read and format test data
     fold1, fold2, fold3 = datagen.collectData("d3&c25&s200")
     train_data, label_data = list(fold1.keys()), list(fold1.values())
@@ -31,14 +34,15 @@ if __name__ == "__main__":
     # Create network
     test_network = nn.NNetwork()
     test_network += l.Flatten(2, (1, 2)) # Input layer
-    test_network += l.Layer(3, alg.identity, alg.q_identity) # Hidden 1 - Identity
-    test_network += l.Layer(3, alg.tanh, alg.q_tanh) # Hidden 2 - Tanh
+    test_network += l.Layer(2, alg.identity, alg.q_identity) # Hidden 1 - Identity
+    # test_network += l.Layer(3, alg.tanh, alg.q_tanh) # Hidden 2 - Tanh
     test_network += l.Layer(1, alg.sigmoid, alg.q_sigmoid) # Output - Sigmoid
 
     # print(test_network.feedforward([-2, 1]))
 
     # Train test network
-    test_network.train(train_data, label_data, alg.mse, alg.q_mse, 5, 1000, report_freq=100)
+    test_network.train(train_data, label_data, alg.mse, alg.q_mse, batch_size=1, n_epochs=25, report_freq=1)
 
     # Evaluate network
-    test_network.evaluate(validata, valilabel, alg.mse)
+    # test_network.evaluate(validata, valilabel, alg.mse)
+    test_network.tell_params()
