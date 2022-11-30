9300f52eb4788551b85b98c5534aa432c091c9dc
orchidObsessed <william.waddell0507@gmail.com>
2022-11-30 16:46:25 -0600

    Begun work on train()

--- neural/network.py
+++ neural/network.py
@@ -2,6 +2,7 @@
 
 # ===== < IMPORTS & CONSTANTS > =====
 from helpers.logsuite import sapilog as sl
+from helpers.algebra import mse, q_mse
 from neural import layer
 import numpy as np
 from random import randint
@@ -211,8 +212,50 @@ class PNetwork:
 
     def train(self, x_set: list[np.ndarray], y_set: list[np.ndarray], batch_size: int, n_epochs: int):
         """
+        Train the network on a given set with contiguous labels.
 
+        Parameters
+        ----------
+        `x_set` : list[np.ndarray]
+            Feature set to train on.
+        `y_set` : list[np.ndarray]
+            Label set to train on (contiguous to `x_set`).
+        `batch_size` : int
+            Number of samples to consider before updating weights.
+        `n_epochs` : int
+            Number of times to repeat training.
         """
+        # Step 1: Select a random sample
+        print(f"xset: {x_set}, yset: {y_set}")
+        r = randint(0, len(x_set)-1)
+        x, y = x_set[r], y_set[r]
+        sl.log(4, f"Considering sample {r}: {x.tolist()} -> {y}")
+
+        # Step 2: Forward propagation, loss
+        y_hat = x # Set "activation" to be input sample
+        for l in self._layers:
+            y_hat = l.activation(y_hat) # Overwrite it with layer's activation
+        loss = mse(y_hat, y)
+        sl.log(4, f"a_L = {y_hat} | loss = {loss}")
+
+        # Step 3: Error and weight gradient for output layer
+        e = np.multiply(self._layers[-1].qa, q_mse(y_hat, y))
+        w_grad = np.asmatrix(e) @ np.asmatrix(self._layers[-2].a.T)
+        sl.log(4, f"Output error: {e} | final layer weight gradient: {w_grad}")
+
+        # Step 4: Backprop error and recalculate weight gradient (up to penultimate)
+        for l in reversed(range(1, len(self._layers)-1)):
+            e = np.multiply(self._layers[l].qa, self._layers[l+1].error_prop(e))
+            w_grad = np.asmatrix(e) @ np.asmatrix(self._layers[l-1].a.T)
+            sl.log(4, f"[Layer {l}] error: {e} | final layer weight gradient: {w_grad}")
+
+        # Step 5: Backprop to the final (first) layer
+        e = np.multiply(self._layers[0].qa, self._layers[1].error_prop(e))
+        sl.log(3, f"e: {e}")
+        sl.log(1, f"Finna multiplty {np.asmatrix(e).shape} by {np.asmatrix(x.T).shape}")
+        w_grad = np.asmatrix(e) @ np.asmatrix(x.T)
+        sl.log(4, f"[Layer {l}] error: {e} | final layer weight gradient: {w_grad}")
+
         return
 # ===== < HELPERS > =====
 
